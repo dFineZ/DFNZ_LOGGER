e93159d72fba3cb5c247686d9774dd112f02ac78
DFNZ Scripts <122015772+dFineZ@users.noreply.github.com>
2025-02-01 09:44:45 +0100

    Update fxmanifest.lua

--- DFNZ_LOGGER/fxmanifest.lua
+++ DFNZ_LOGGER/fxmanifest.lua
@@ -2,10 +2,13 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-author 'DFNZ'
+author 'DFNZscripts'
 description 'Webhook Script by DFNZscripts'
-version '1.0.1'
+version '1.1.0'
 
 server_scripts {
+    'shared/config.lua',
     'server/*.lua'    
 }
+
+server_export 'sendHook'
