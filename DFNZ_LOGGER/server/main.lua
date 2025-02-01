acf006876bec4cb5ec5a3a0dd0a8135d43cb3690
DFNZ Scripts <122015772+dFineZ@users.noreply.github.com>
2025-02-01 09:42:19 +0100

    Update main.lua

--- DFNZ_LOGGER/server/main.lua
+++ DFNZ_LOGGER/server/main.lua
@@ -1,17 +1,48 @@
+local L = Config.Language
+
+-- join
+AddEventHandler("playerConnecting", function(name, setKickReason, deferrals)
+    if Config.ForceDiscord then 
+        local discord = GetPlayerIdentifierByType(source, 'discord')
+        if discord == nil then
+            CancelEvent()
+            setKickReason("\n \n"..Config.Text[L].no_discord)
+        end
+    end
+
+    if not Config.BasicLogs.join then return end
+    local url = ''
+    sendHook(url, '', '', source)
+end)
+
+-- leave
+AddEventHandler('playerDropped', function (reason)
+    if not Config.BasicLogs.leave then return end
+    local url = ''
+    sendHook(url, Config.Text[L].reason..reason, '', source)
+end)
+
+-- chat
+AddEventHandler('chatMessage', function(source, Name, message)
+    if not Config.BasicLogs.chat then return end
+    local url = ''
+    sendHook(url, Config.Text[L].chat, message, source)
+end)
+
 function sendHook(url, name, message, id)
+    local image = ''
     if id ~= nil then
+        local options = {}
+        local identifiers = getIdentifier(id)
+
         local embed = {
             {
                 ["color"] = 16777215,
-                ["title"] = "**"..name.."**",
+                ["title"] = name,
                 ["description"] = message,
-                fields = {
-                    {name = "ID", value = id, inline = true},
-                    {name = "Discord", value = '<@'..GetPlayerIdentifierByType(id, 'discord'):gsub('discord:', '')..'>', inline = true},
-                    {name = "Ping", value = GetPlayerPing(id), inline = true},
-                },
+                fields = identifiers,
                 ["footer"] = {
-                    ["text"] = 'Logger by DFNZscripts',
+                    ["text"] = '©DFNZscripts \n'..os.date("%d/%m/%Y %X"),
                 },
             }
         }
@@ -23,7 +54,7 @@ function sendHook(url, name, message, id)
                 ["title"] = "**"..name.."**",
                 ["description"] = message,
                 ["footer"] = {
-                    ["text"] = 'Logger by DFNZscripts',
+                    ["text"] = '©DFNZscripts \n'..os.date("%d/%m/%Y %X"),
                 },
             }
         }
@@ -31,4 +62,33 @@ function sendHook(url, name, message, id)
     end   
 end
 
-exports('sendHook', sendHook)
+function getIdentifier(id)
+    local data = {}
+
+    if Config.LogsInformation.id then 
+        data[#data + 1] = {name = '`👤` ID', value = id, inline = true}
+    end
+
+    if Config.LogsInformation.discord then
+        local discord = GetPlayerIdentifierByType(id, 'discord')
+        if discord == nil then
+            data[#data + 1] = {name = '`📌` DISCORD', value = 'N/A', inline = true}
+        else
+            data[#data + 1] = {name = '`📌` DISCORD', value = '<@'..discord:gsub('discord:', '')..'>', inline = true}
+        end
+    end
+
+    if Config.LogsInformation.ping then 
+        data[#data + 1] = {name = '`📶` PING', value = GetPlayerPing(id)..'ms', inline = true}
+    end
+
+    if Config.LogsInformation.ip then
+        data[#data + 1] = {name = '`🌐` IP ADRESSE', value = '||'..GetPlayerEndpoint(id)..'||', inline = true}
+    end
+
+    if Config.LogsInformation.fivem then
+        data[#data + 1] = {name = '`🟧` FIVEM', value = '||'..GetPlayerIdentifierByType(id, 'fivem')..'||', inline = true}
+    end
+
+    return data
+end
